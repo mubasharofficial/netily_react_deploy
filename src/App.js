e573eb0c2dab8c29ea5174074570126e78fa6122
mubashar <mubashar.hussain22@gmail.com>
2022-01-28 10:46:39 +0500

    finalized our app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import './App.css';
-
+import React , {useEffect,useState} from 'react';
 function App() {
+
+  // const [val,setval] = useState({});
+
+  // useEffect(()=>{
+  //   fetch('/.netlify/functions/hello?name=frome new react app')
+  //   .then(res=>res.json())
+  //   .then(obj=>setval(obj))
+  // })
   return (
     <div className="App">
       <h1>REACT HELLO WORD by REACT
